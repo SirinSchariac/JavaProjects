af2ee6be57609cdd484fbbfd596bdf49c9389c2f
SirinSchariac <106457519+SirinSchariac@users.noreply.github.com>
2025-03-20 16:07:20 +0800

    upload JDK test file

---
+++ Project0/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args)
+    {
+        System.out.println("This is a test for Java Env");
+        System.out.println("This msg means successful run");
+    }
+    
+}
